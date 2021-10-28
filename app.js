e42f3b932635b7fb47683398250afdb7a74af52c
Jobin Mathew <72184870+codfishy@users.noreply.github.com>
2021-10-28 20:27:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var typed=new Typed(".typing", {
+    strings:["Photographer","Developer","Designer","Editor"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+
+
+var h=document.getElementsByTagName("head")[0];
+var s=document.createElement("script");
+s.type="text/javascript";
+s.setAttribute('data-key', "wXHPLtiAQm-tjDQ7-c9g2Q==");
+s.src="https://widgets.mailcastr.com/release/bot/v1/mcbot.min.js";
+s.async = true;
+h.appendChild(s);
